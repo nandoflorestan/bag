4ea6214f2e51ab66ab421130a763d56cd8e92d7a
Nando Florestan <nandoflorestan@gmail.com>
2012-08-24 23:20:37 -0300

    The contact form wouldn't appear anymore. Fixed.

--- bag/web/pyramid/apps/contact/__init__.py
+++ bag/web/pyramid/apps/contact/__init__.py
@@ -2,21 +2,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import (absolute_import, division, print_function,
     unicode_literals)
-from pyramid.view import view_config, view_defaults
 import colander as c
 import deform as d
-from marrow.mailer import Message
 from ... import _
 from ...starter import register_view_class
 from ..views import BaseView
 
 
-def create_contact_view(BaseView=BaseView, route_name='contact',
-        action='', renderer=None, master_template='aaa.genshi',
-        len_name=dict(min=2, max=200),
-        len_subject=dict(max=256),
-        len_message=dict(max=80 * 500),
-        subject_prefix='[Contact form]', impersonate=False):
+def create_contact_view(config, BaseView=BaseView, route_name='contact',
+    action='', renderer=None, master_template='aaa.genshi',
+    len_name=dict(min=2, max=200),
+    len_subject=dict(max=256),
+    len_message=dict(max=80 * 500),
+    subject_prefix='[Contact form]', impersonate=False):
     '''Returns a generic view for a contact form page which sends email
     using marrow.mailer.
 
@@ -30,7 +28,7 @@ def create_contact_view(BaseView=BaseView, route_name='contact',
         from bag.web.pyramid.apps.contact import create_contact_view
         ContactView = create_contact_view()
 
-    "action" defines where the form POSTs to.
+    *action* defines where the form POSTs to.
     '''
     if not renderer:  # No renderer was passed, so use our default template
         import os
@@ -38,11 +36,14 @@ def create_contact_view(BaseView=BaseView, route_name='contact',
         renderer = here + '/contact.genshi'
 
     @register_view_class
-    @view_defaults(route_name=route_name, renderer=renderer)
     class ContactView(BaseView):
         @staticmethod
-        def declare_routes(config, prefix='/'):
+        def declare_routes(config, prefix=''):
             config.add_route(route_name, prefix + 'contact')
+            config.add_view(route_name=route_name, view=ContactView,
+                renderer=renderer, request_method='GET', attr='get')
+            config.add_view(route_name=route_name, view=ContactView,
+                renderer=renderer, request_method='POST', attr='post')
 
         def __init__(self, request):
             self.request = request
@@ -55,12 +56,10 @@ def create_contact_view(BaseView=BaseView, route_name='contact',
             return dict(pagetitle=_("Contact"), contact_form=contact_form,
                         master_template=master_template)
 
-        @view_config(request_method='GET')
         def get(self):
             '''Displays the contact form.'''
             return self._template_context(self._get_form().render())
 
-        @view_config(request_method='POST')
         def post(self):
             '''Sends out an email if POSTed data validates;
             else redisplays the form with the error messages.
@@ -75,24 +74,18 @@ def create_contact_view(BaseView=BaseView, route_name='contact',
             self._send(**appstruct)
             return self._on_success()
 
-        msg_template = """Message sent via contact form by:
-{name} <{email}>
-Subject: {subject}
-
-{message}"""
-
         def _assemble_msg(self, **kw):
-            return self.msg_template.format(**kw)
+            return msg_template.format(**kw)
 
         def _send(self, **k):
             if impersonate:
-                msg = self.request.registry.mailer.new \
-                    (author=(k['name'], k['email']),
+                msg = self.request.registry.mailer.new(
+                    author=(k['name'], k['email']),
                     plain=self._assemble_msg(**k),
                     subject='{} {}'.format(subject_prefix, k['subject']))
             else:  # Author will come from configuration
-                msg = self.request.registry.mailer.new \
-                    (plain=self._assemble_msg(**k),
+                msg = self.request.registry.mailer.new(
+                    plain=self._assemble_msg(**k),
                     subject='{} {}'.format(subject_prefix, k['subject']))
             msg.send()
 
@@ -112,13 +105,19 @@ Subject: {subject}
                 widget=d.widget.TextInputWidget(size=60, maxlength=160))
             message = c.SchemaNode(c.Str(), title=_('Message'),
                     widget=d.widget.TextAreaWidget(cols=60, rows=12))
+
         if len_name:
             ContactSchema.name.widget.maxlength = len_name.get('max')
             ContactSchema.name.validator = c.Length(**len_name)
         if len_subject:
-            ContactSchema.subject.validator=c.Length(**len_subject)
+            ContactSchema.subject.validator = c.Length(**len_subject)
         if len_message:
-            ContactSchema.message.validator=c.Length(**len_message)
-
-
+            ContactSchema.message.validator = c.Length(**len_message)
     return ContactView
+
+
+msg_template = """Message sent via contact form by:
+{name} <{email}>
+Subject: {subject}
+
+{message}"""
