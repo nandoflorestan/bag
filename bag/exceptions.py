33c55e769d4733de3ac1661f7d2eacc9812c3f49
Nando Florestan <nandoflorestan@gmail.com>
2012-10-03 11:40:34 -0300

    Add bag/exceptions.py

---
+++ bag/exceptions.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function,
+    unicode_literals)
+
+
+class ArgumentError(ValueError):
+    def __init__(self, arg, val):
+        self.arg = arg
+        self.val = val
+        val = str(val)
+        if len(val) > 43:
+            val = val[:40] + '...'
+        super(ArgumentError, self).__init__(
+            'Argument "{}" does not accept value "{}"'.format(arg, val))
