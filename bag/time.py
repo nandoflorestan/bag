b97978cdd3ceadf74e72c5e0df915f7cbaf8a8fc
Nando Florestan <nandoflorestan@gmail.com>
2012-10-13 11:03:01 -0300

    Add timed_call()

--- bag/time.py
+++ bag/time.py
@@ -5,7 +5,8 @@
 
 from __future__ import (absolute_import, division, print_function,
     unicode_literals)
-from datetime import datetime
+from datetime import datetime, timedelta
+from time import sleep
 
 
 def simplify_datetime(val, granularity='minute'):
@@ -17,3 +18,26 @@ def simplify_datetime(val, granularity='minute'):
             val.second)
     if granularity == 'hour':
         return datetime(val.year, val.month, val.day, val.hour)
+
+
+def timed_call(seconds, function, repetitions=-1, *a, **kw):
+    '''Performs some task every x seconds. Sleeps if necessary.
+    Does not sleep after the last turn.
+
+    By default, runs forever. To control the number of times
+    that *function* should run, pass in a number of *repetitions*.
+    Returns immediately if *repetitions* is zero.
+    '''
+    period = seconds if isinstance(seconds, timedelta) else \
+        timedelta(0, seconds)
+    turn = 0
+    while True:
+        if turn == repetitions:
+            return
+        if repetitions > -1:
+            turn += 1
+        started = datetime.utcnow()
+        function(*a, **kw)
+        took = datetime.utcnow() - started
+        if turn != repetitions and took < period:
+            sleep((period - took).total_seconds())
