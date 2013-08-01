4a2b6db231a607a481484bad41adfccbb74a72e2
Nando Florestan <nandoflorestan@gmail.com>
2013-08-01 08:34:02 -0300

    setup_log() now supports a single log file, too

--- bag/log.py
+++ bag/log.py
@@ -10,13 +10,20 @@ from logging.handlers import RotatingFileHandler
 from nine import basestring
 
 
-def setup_log(name=None, directory='logs', backups=3,
-              level=logging.DEBUG, screen_level=logging.INFO):
+def setup_log(name=None, path='logs', rotating=True, backups=3, file_mode='a',
+              disk_level=logging.DEBUG, screen_level=logging.INFO,
+              encoding='utf-8'):
     '''This logs to screen if ``screen_level`` is not None, and logs to
-    a scheme of rotating files if ``level`` is not None.
+    disk if ``disk_level`` is not None.
 
     If you do not pass a log ``name``, the root log is configured and
     returned.
+
+    If ``rotating`` is True, a RotatingFileHandler is configured on the
+    directory passed as ``path``.
+
+    If ``rotating`` is False, a single log file will be created at ``path``.
+    Its ``file_mode`` defaults to `a` (append), but you can set it to `w`.
     '''
     # If strings are passed in as levels, "decode" them first
     levels = dict(
@@ -26,25 +33,28 @@ def setup_log(name=None, directory='logs', backups=3,
         error=logging.ERROR,                            # 40
         critical=logging.CRITICAL, fatal=logging.FATAL,  # 50
     )
-    if isinstance(level, basestring):
-        level = levels[level.lower()]
+    if isinstance(disk_level, basestring):
+        disk_level = levels[disk_level.lower()]
     if isinstance(screen_level, basestring):
         screen_level = levels[screen_level.lower()]
     # Set up logging
     log = logging.getLogger(name)
-    if level:
-        try:
-            os.mkdir(directory)
-        except OSError:
-            pass
-        h1 = RotatingFileHandler(os.path.join(directory,
-            name or 'root' + ".log.txt"),
-            maxBytes=2 ** 22, backupCount=backups)
-        h1.setLevel(level)
-        log.setLevel(level)
-        log.addHandler(h1)
     if screen_level:
-        h2 = logging.StreamHandler()
-        h2.setLevel(screen_level)
+        h1 = logging.StreamHandler()
+        h1.setLevel(screen_level)
+        log.addHandler(h1)
+    if disk_level:
+        if rotating:
+            try:
+                os.mkdir(path)
+            except OSError:
+                pass
+            h2 = RotatingFileHandler(os.path.join(path,
+                name or 'root' + ".log.txt"), encoding=encoding,
+                maxBytes=2 ** 22, backupCount=backups)
+        else:
+            h2 = logging.FileHandler(path, mode=file_mode, encoding=encoding)
+        h2.setLevel(disk_level)
+        log.setLevel(disk_level)
         log.addHandler(h2)
     return log
