c567481f7a2f1aedd05c626c3d54e8c6af5d8c5a
Nando Florestan <nandoflorestan@gmail.com>
2013-10-26 15:39:35 -0200

    Version 0.3.9

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url='https://github.com/nandoflorestan/bag',
     name="bag",
     author='Nando Florestan',
-    version='0.3.9dev',
+    version='0.3.9',
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
