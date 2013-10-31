955ece491ec21cb9b6df5083e3484b904766d223
Nando Florestan <nandoflorestan@gmail.com>
2013-10-31 11:39:56 -0200

    Bump version to 0.3.12dev after release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url='https://github.com/nandoflorestan/bag',
     name="bag",
     author='Nando Florestan',
-    version='0.3.11',
+    version='0.3.12dev',
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
