a19f7cecea5a1efaf9a289d5adfed060cff2d2e6
Nando Florestan <nandoflorestan@gmail.com>
2013-11-27 15:22:04 -0200

    Add asbool()

--- bag/__init__.py
+++ bag/__init__.py
@@ -2,3 +2,15 @@
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+
+_boolean_states = {'1': True, 'yes': True, 'true': True, 'on': True,
+                   '0': False, 'no': False, 'false': False, 'off': False}
+
+
+def asbool(thing):
+    if thing is None or isinstance(thing, (bool, int)):
+        return thing
+    val = _boolean_states.get(thing.lower())
+    if val is None:
+        raise ValueError('Not a boolean: "%s"' % thing)
+    return val
