fdea6cd6946d86bbe0b3ca0ad927d7f5011cdad7
Nando Florestan <nandoflorestan@gmail.com>
2014-01-03 22:47:18 -0200

    Version 0.4.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url='https://github.com/nandoflorestan/bag',
     name="bag",
     author='Nando Florestan',
-    version='0.4.1dev',
+    version='0.4.1',
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
