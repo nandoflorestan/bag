f15be51246b7c974a2f0d8983b814a3d74f7daef
Nando Florestan <nandoflorestan@gmail.com>
2014-10-16 23:20:45 -0300

    Bump version to 0.4.4dev after release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     url='https://github.com/nandoflorestan/bag',
     name="bag",
     author='Nando Florestan',
-    version='0.4.3',
+    version='0.4.4dev',
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
