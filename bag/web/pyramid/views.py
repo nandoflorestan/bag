92725012f1629edd1e39e7419aa1cb24989581af
Nando Florestan <nandoflorestan@gmail.com>
2015-01-21 18:39:37 -0200

    get_json_or_raise() can validate the payload type

--- bag/web/pyramid/views.py
+++ bag/web/pyramid/views.py
@@ -5,11 +5,19 @@ from __future__ import (absolute_import, division, print_function,
 from pyramid.httpexceptions import HTTPBadRequest
 
 
-def get_json_or_raise(request):
+def get_json_or_raise(request, expect=None):
     '''If the json cannot be decoded, this is a bad request, so raise 400
         instead of 500.
+
+        The decoded json may be of a number of types. You can validate the
+        type, passing an ``expect`` argument. If the json decodes
+        to the wrong type, also raise 400 instead of 500.
         '''
     try:
-        return request.json_body
+        payload = request.json_body
     except ValueError as e:
         raise HTTPBadRequest(detail=str(e))
+    if expect is not None and not isinstance(payload, expect):
+        raise HTTPBadRequest(
+            detail='Expected {}, got {}'.format(expect, type(payload)))
+    return payload
