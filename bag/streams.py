b251ff60e323a998b22de76ff8a58f35741d3a96
Nando Florestan <nandoflorestan@gmail.com>
2015-02-13 16:26:18 -0200

    Add bag.streams.get_file_size(stream)

---
+++ bag/streams.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+'''Functions that use streams (open files)'''
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+
+def get_file_size(stream):
+    assert stream.seekable(), 'Streams of type {} are not seekable'.format(
+        type(stream))
+    stream.seek(0, 2)  # Seek to the end of the stream
+    size = stream.tell()  # Get the position of EOF
+    stream.seek(0)  # Reset the stream position to the beginning
+    return size
