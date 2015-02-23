0441a9924404adce43bd6b63cdf36d13c1e880fa
Nando Florestan <nandoflorestan@gmail.com>
2015-02-22 22:36:06 -0300

    Add @ajax_view and the Problem exception

---
+++ bag/web/exceptions.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from nine import nine
+
+
+@nine
+class Problem(Exception):
+    '''Service (or "action") layers should be independent of web frameworks;
+        however, often I feel I want the service layer to determine the
+        HTTP code of a web service, instead of the controller layer.
+        So I raise this exception in the service layer.
+
+        Very useful when coupled with the
+        `ajax_view decorator <https://github.com/nandoflorestan/bag/blob/master/bag/web/pyramid/views.py>`_.
+        '''
+
+    HTTP = {  # http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+        # TODO Complete this map with the remaining 4xx and 5xx
+        # 4xx Client error
+        400: 'Bad request',
+        401: 'Unauthorized',
+        403: 'Forbidden',
+        404: 'Not found',
+        409: 'Conflict',
+        410: 'Gone',
+        413: 'Request entity too large',
+        422: 'Unprocessable entity',
+        # 5xx Server error
+        500: 'Internal server error',
+        }
+
+    def __init__(self, msg, http_code=500, error_type=None, **kw):
+        self.http_code = int(http_code)
+        kw['error_type'] = error_type or self.HTTP[self.http_code]
+        kw['error_msg'] = msg
+        self.kw = kw
+
+    def to_dict(self):
+        return self.kw
+
+    def __repr__(self):
+        return '<{} {}>'.format(type(self).__name__, self.kw)
+
+    def __str__(self):
+        return self.kw['error_msg']
--- bag/web/pyramid/views.py
+++ bag/web/pyramid/views.py
@@ -2,7 +2,11 @@
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-from pyramid.httpexceptions import HTTPBadRequest
+from functools import wraps
+from json import dumps
+from bag.web.exceptions import Problem
+from pyramid.httpexceptions import HTTPBadRequest, HTTPError
+from pyramid.response import Response
 
 
 def get_json_or_raise(request, expect=None):
@@ -21,3 +25,41 @@ def get_json_or_raise(request, expect=None):
         raise HTTPBadRequest(
             detail='Expected {}, got {}'.format(expect, type(payload)))
     return payload
+
+
+def ajax_view(view_function):
+    '''Decorator for AJAX views that grabs certain exceptions and turns them
+        into an error JSON response that contains:
+
+        - "error_msg": the string to be displayed to end users
+        - "error_type": a sort of title of the error
+        - possibly other variables, too
+
+        The transaction is not committed because we **raise** HTTPError.
+        '''
+    @wraps(view_function)
+    def wrapper(context, request):
+        try:
+            o = view_function(context, request)
+            # If *o* is a model instance, convert it to a dict.
+            return o if isinstance(o, (Response, dict)) else o.to_dict()
+        except Problem as e:
+            comment = 'Problem found in action layer'
+            http_code = e.http_code
+            error_dict = e.to_dict()
+        except Exception as e:
+            if hasattr(e, 'asdict') and callable(e.asdict):
+                comment = 'Colander validation error'
+                http_code = 422  # Unprocessable Entity
+                error_dict = e.asdict()
+                error_dict['error_type'] = 'Invalid'
+            else:
+                raise  # Let this view-raised exception pass through
+        raise HTTPError(
+            status_int=http_code,
+            content_type='application/json',
+            body=dumps(error_dict),
+            detail=error_dict['error_msg'],  # could be shown to end users
+            comment=comment,  # not displayed to end users
+            )
+    return wrapper
