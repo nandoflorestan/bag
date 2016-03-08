5b7c031eb0e92dcf94676bd70b6a4ef6e79ded23
Nando Florestan <nandoflorestan@gmail.com>
2016-03-08 11:55:13 -0300

    Burla supports URL params and fragments (in JS too)

--- bag/web/burla.py
+++ bag/web/burla.py
@@ -1,6 +1,52 @@
 # -*- coding: utf-8 -*-
 
-"""Burla: Powerful URL generation independent of web frameworks"""
+"""Burla: Powerful URL generation independent of web frameworks.
+
+    Burla stores a collection of page URL templates, separate from a
+    collection of API method URL templates.  The only difference between
+    them is that operations have a request method (GET, POST, PUT etc.).
+
+    Burla also facilitates generating documentation about pages and
+    API operations in the Python server.
+
+    No matter what web framework you are using, you are better off
+    generating URLs with Burla because this makes your application more
+    independent of web frameworks so you can switch more easily.
+
+    Another advantage is that, based on the URL templates, burla generates
+    URLs in the Python server as well as in the Javascript client.  It
+    generates a short Javascript library that takes care of this.
+
+    Maintenance of your web app becomes easier because you register your
+    URLs only once (in the Python server code) and then the URLs can be
+    used in the entire stack.  When you change your URLs, you only do it
+    in one place.
+
+    URL templates (for matching views) stop at the left of the question mark,
+    but when generating URLs, burla supports both query params
+    (to the right of the question mark) and fragments (to the right of the #),
+
+    Here are a few examples of usage in the Javascript client::
+
+        // Let's see a previously registered URL template:
+        burla.page('User details')
+        "/users/:user_id/details"
+
+        // Provide a map to generate a real URL from the template:
+        burla.page('User details', {user_id: 1})
+        "/users/1/details"
+
+        // Provide another argument to add a fragment (to the right of the #):
+        burla.page('User details', {user_id: 1}, 'tab=aboutme')
+        "/users/1/details#tab=aboutme"
+
+        // Extra params (not found in the URL template) go in the query:
+        burla.page('User details', {user_id: 1, photos:'big'}, 'tab=aboutme')
+        "/users/1/details?photos=big#tab=aboutme"
+
+    You can find integration with the Pyramid web framework in the module
+    bag.web.pyramid.burla. Please contribute integration into other frameworks.
+    """
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
@@ -224,31 +270,44 @@ class Burla(object):
         return """"use strict";
 
 // Usage:
-// var url = burla.page(pageName, urlParams);
-// var url = burla.op(operationName, urlParams);
+// var url = burla.page(pageName, params, fragment);
+// var url = burla.op(operationName, params, fragment);
 
 window.burla = {
     root: ROOT,
     pages: PAGES,
     ops: OPERATIONS,
 
-    _find: function (map, name, options) {
+    urlencode: function (adict) {
+        return Object.keys(adict).map(function (key) {
+            return [key, adict[key]].map(encodeURIComponent).join("=");
+        }).join("&");
+    },
+    _find: function (map, name, params, fragment) {
         var s = map[name].url_templ;
         if (!s)  throw new Error('burla: No item called "' + name + '".');
-        for (var key in options) {
+        var p = {};
+        for (var key in params) {
             var placeholder = ':' + key;
+            var val = params[key];
             if (s.indexOf(placeholder) == -1) {
-                throw new Error('burla: URL template "' + name + '" does not use parameter "' + key + '".');
+                p[key] = val; // accumulate
+            } else {
+                if (val == null)  throw new Error('burla: Operation "' + name + '" needs parameter "' + key + '".');
+                s = s.replace(placeholder, val);
             }
-            var val = options[key];
-            if (val == null)  throw new Error('burla: Operation "' + name + '" needs parameter "' + key + '".');
-            s = s.replace(placeholder, val);
         }
+        var strParams = this.urlencode(p);
+        if (strParams) s += '?' + strParams;
+        if (fragment)  s += '#' + fragment;
         return this.root + s;
     },
-
-    page: function (name, options) { return this._find(this.pages, name, options); },
-    op: function (name, options) { return this._find(this.ops, name, options); },
+    page: function (name, params, fragment) {
+        return this._find(this.pages, name, params, fragment);
+    },
+    op: function (name, params, fragment) {
+        return this._find(this.ops, name, params, fragment);
+    },
 }\n""" \
             .replace('PAGES', dumps(
                 {o.name: o.to_dict() for o in self.gen_pages()},
