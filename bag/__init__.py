891fc695649312b609eb510402147a21ee9247cc
Nando Florestan <nandoflorestan@gmail.com>
2016-05-25 14:04:21 -0300

    Improve documentation on the bag module

--- bag/__init__.py
+++ bag/__init__.py
@@ -14,6 +14,7 @@ _boolean_states = {'1': True, 'yes': True, 'true': True, 'on': True,
 
 
 def asbool(thing):
+    """Convert a configuration value to a boolean."""
     if thing is None or isinstance(thing, (bool, int)):
         return thing
     val = _boolean_states.get(thing.lower())
@@ -23,9 +24,10 @@ def asbool(thing):
 
 
 def resolve(resource_spec):
-    """Returns the variable referred to in a string such as
-        ``my.python.module:some_variable``.
-        """
+    """Return the variable referred to in the ``resource_spec`` string.
+
+    Example resource_spec: ``"my.python.module:some_variable"``.
+    """
     if isinstance(resource_spec, ModuleType):  # arg is a python module
         return resource_spec
     module, var = resource_spec.split(':')  # arg is assumed to be a string
@@ -34,11 +36,12 @@ def resolve(resource_spec):
 
 
 def resolve_path(resource_spec):
-    """Returns the path referred to in a string such as
-        ``my.python.module:some/subdirectory``, as a pathlib.Path object.
+    """Return a pathlib.Path corresponding to the ``resource_spec`` string.
 
-        Similar: from pyramid.resource import abspath_from_asset_spec
-        """
+    Example argument: ``"my.python.module:some/subdirectory"``
+
+    Similar: ``from pyramid.resource import abspath_from_asset_spec``
+    """
     from pathlib import Path
     module, var = resource_spec.split(':')  # arg is assumed to be a string
     module = import_module(module)
@@ -46,12 +49,14 @@ def resolve_path(resource_spec):
 
 
 def first(iterable):
+    """Return the first object in ``iterable``."""
     for o in iterable:
         return o
 
 
 def dict_subset(adict, predicate):
-    """Given a dictionary and a predicate function, returns another dictionary
-        that contains only a subset of the keys in the original.
-        """
+    """Return a dict that is a subset of ``adict`` using a filter function.
+
+    The signature of the filter is: ``predicate(key, val) -> bool``
+    """
     return {k: v for k, v in adict.items() if predicate(k, v)}
