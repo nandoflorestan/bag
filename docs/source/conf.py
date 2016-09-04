57f5378e17f1d82fd0523b1f7cb10e9e2512294b
Nando Florestan <nandoflorestan@gmail.com>
2016-09-04 02:05:10 -0300

    Try to make autodoc work on readthedocs.org

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,7 @@ def get_version_number(path=''):
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration ------------------------------------------------
 
