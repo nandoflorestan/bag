0c1558dcfc3a7b4f7d5ea2b1f743be88debf056e
Nando Florestan <nandoflorestan@gmail.com>
2016-10-14 14:59:50 -0300

    Pyramid 1.7+ makes our @csrf decorator obsolete

--- bag/web/pyramid/angular_csrf.py
+++ bag/web/pyramid/angular_csrf.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-"""Make Pyramid play ball with AngularJS to achieve CSRF protection.
+"""Make Pyramid 1.7+ play ball with AngularJS to achieve CSRF protection.
 
-To start using this module, include it in your application configuration::
+To use this module, include it in your application configuration::
 
     # Integrate with Angular for CSRF protection:
     config.include('bag.web.pyramid.angular_csrf')
@@ -13,32 +13,20 @@ cookie containing the CSRF token, just as Angular 1.3.x wants it.
 In subsequent AJAX requests (with verbs different than GET),
 Angular will send the token back in a header 'X-XSRF-Token'.
 
-Now decorate your views with ``@csrf`` and they will raise HTTPForbidden when
-the token is missing from the request. Usage::
-
-    from bag.web.pyramid.angular_csrf import csrf
-
-    @view_config(context=User, permission='edit_user',
-                 accept='application/json', request_method='PUT',
-                 renderer='json')
-    @csrf
-    def view_that_changes_a_user(context, request):
-        ...
-
-Although I haven't tested this, I hear one can also decorate a class::
-
-    @view_defaults(decorator=csrf)
-    class SomeView(object):
-        ...
+Pyramid will validate the CSRF token before calling your view handler.
 
 Relevant angular documentation is at
 https://docs.angularjs.org/api/ng/service/$http
+
+Relevant Pyramid documentation is at
+http://docs.pylonsproject.org/projects/pyramid/en/latest/narr/sessions.html#checking-csrf-tokens-automatically
 """
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from functools import wraps
 from pyramid.events import NewResponse
+from pyramid.exceptions import BadCSRFToken
 from pyramid.httpexceptions import HTTPForbidden
 from . import _
 
@@ -59,21 +47,7 @@ def on_GET_request_setup_csrf_cookie(ev):
             ev.response.set_cookie(COOKIE_NAME, token, overwrite=True)
 
 
-def csrf(fn):
-    """Decorator that protects a view handler against CSRF attacks."""
-    @wraps(fn)
-    def wrapper(context, request):
-        token = request.headers.get(HEADER_NAME)
-        session_token = request.session.get_csrf_token()
-        # print(token, session_token)
-        if token == session_token:
-            return fn(context, request)
-        else:
-            raise HTTPForbidden(_(
-                'Invalid CSRF token. Please try reloading the page.'))
-    return wrapper
-
-
 def includeme(config):
     """Set up the angular_csrf protection scheme for a Pyramid application."""
     config.add_subscriber(on_GET_request_setup_csrf_cookie, NewResponse)
+    config.set_default_csrf_options(require_csrf=True, header=HEADER_NAME)
