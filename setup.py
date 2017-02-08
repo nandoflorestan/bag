894c58e2f4e0293a2419cb5d31a8b16f4ad6712d
Nando Florestan <nandoflorestan@gmail.com>
2017-02-08 19:28:35 -0200

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     url='https://github.com/nandoflorestan/bag',
     name="bag",
     author='Nando Florestan',
-    version='0.9.1.dev1',
+    version='1.0.0',
     license='MIT',
     # https://setuptools.readthedocs.io/en/latest/setuptools.html#using-find-packages
     packages=find_packages(exclude=["tests.*", 'tests']),
