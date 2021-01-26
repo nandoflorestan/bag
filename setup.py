22f50e0532abc1b95c70e0ab4156a10e86591bac
Nando Florestan <nandoflorestan@gmail.com>
2021-01-26 20:16:18 +0100

    Bump version to 3.1.1.dev1 after release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     url='https://github.com/nandoflorestan/bag',
     name="bag",
     author='Nando Florestan',
-    version='3.1.0',
+    version='3.1.1.dev1',
     license='MIT',
     # https://setuptools.readthedocs.io/en/latest/setuptools.html#using-find-packages
     packages=find_packages(exclude=["tests.*", 'tests']),
