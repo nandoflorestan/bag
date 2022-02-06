e0bfe84446e69058e0e84022180160d877e088aa
Nando Florestan <nandoflorestan@gmail.com>
2022-02-06 16:27:38 +0100

    Bump version to 5.0.1.dev1 after release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url='https://github.com/nandoflorestan/bag',
     name="bag",
     author='Nando Florestan',
-    version='5.0.0',
+    version='5.0.1.dev1',
     license='MIT',
     # https://setuptools.readthedocs.io/en/latest/setuptools.html#using-find-packages
     packages=find_packages(exclude=["tests.*", 'tests']),
